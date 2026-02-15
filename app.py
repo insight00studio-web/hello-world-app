0136eee55418383d324187f1bb9d0add7b309dfa
Your Name <you@example.com>
2026-02-15 17:29:07 +0900

    style: increase font size and set light blue background

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World'
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    # Renderなどの環境でポート番号が指定される場合に対応
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
